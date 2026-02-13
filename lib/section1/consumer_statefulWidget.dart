0bc836c4069c3a7c3b59b21cbc2cfb534a916a45
Anant <anantsingh.code@mail.com>
2026-02-13 07:20:08 +0530

    Stateful provider

--- lib/section1/consumer_statefulWidget.dart
+++ lib/section1/consumer_statefulWidget.dart
@@ -1,16 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
+
+final textProvider = StateProvider((Ref ref) {
+  return '';
+});
 
 class ConsumerStatefulwidget extends ConsumerStatefulWidget {
   const ConsumerStatefulwidget({super.key});
 
   @override
-  ConsumerState<ConsumerStatefulwidget> createState() => _ConsumerStatefulwidgetState();
+  ConsumerState<ConsumerStatefulwidget> createState() =>
+      _ConsumerStatefulwidgetState();
 }
 
-class _ConsumerStatefulwidgetState extends ConsumerState<ConsumerStatefulwidget> {
+class _ConsumerStatefulwidgetState
+    extends ConsumerState<ConsumerStatefulwidget> {
+  late final TextEditingController _controller;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _controller = TextEditingController();
+    _controller.addListener(() {
+      ref.read(textProvider.notifier).state = _controller.text;
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final text = ref.watch(textProvider);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Consumer Stateful Provider')),
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              TextFormField(controller: _controller),
+              const SizedBox(height: 20),
+              Text('You Typed: $text'),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
